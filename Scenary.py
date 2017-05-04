4241efc854d22cf0ce8faef4cf55f5af7103ff6f
lapetre <lucian_apetre@yahoo.com>
2017-05-04 10:59:22 +0300

    Added hover and out on ButtonBase

--- Scenary.py
+++ Scenary.py
@@ -23,15 +23,21 @@ class Platform(pygame.sprite.Sprite):
 
 class ButtonBase(pygame.sprite.Sprite):
 
-    def __init__(self, sprite_sheet_data):
+    def __init__(self, sprite_sheet_data, sprite_sheet_data_hover):
         pygame.sprite.Sprite.__init__(self)
         sprite_sheet = SpriteSheet("resources/Menu_Items.png")
 
-        self.image = sprite_sheet.get_image(sprite_sheet_data[0],
+        self.image_out = sprite_sheet.get_image(sprite_sheet_data[0],
                                            sprite_sheet_data[1],
                                            sprite_sheet_data[2],
                                            sprite_sheet_data[3])
-        self.rect = self.image.get_rect()
+
+        self.image_hover = sprite_sheet.get_image(sprite_sheet_data_hover[0],
+                                                  sprite_sheet_data_hover[1],
+                                                  sprite_sheet_data_hover[2],
+                                                  sprite_sheet_data_hover[3])
+        self.rect = self.image_out.get_rect()
+        self.image = self.image_out
 
         #mouse over flags
         self.is_mouse_over = False
@@ -45,18 +51,18 @@ class ButtonBase(pygame.sprite.Sprite):
 
         if self.is_mouse_over == True:
             if self.mouse_over_histoy == False:
-                #draw hover image
+                self.image = self.image_hover
                 self.mouse_over_histoy = True
         else:
             if self.mouse_over_histoy == True:
+                self.image = self.image_out
                 self.mouse_over_histoy = False
-                #draw hover out image
 
 
 class PlayButton(ButtonBase):
 
     def __init__(self, x, y):
-        ButtonBase.__init__(self, localtypes.PLAY_BUTTON)
+        ButtonBase.__init__(self, localtypes.PLAY_BUTTON, localtypes.HEART_BUTTON)
         self.rect.x = x
         self.rect.y = y
         self.play_button = pygame.sprite.GroupSingle(self)
---
+++ resources/24 Game Buttons/ReadMe.txt
@@ -0,0 +1,43 @@
+These free files are made by pixaroma.com
+
+If you agree to use our website, then you have accepted to abide by all the rules and regulations, Terms and conditions that govern www.pixaroma.com which are subjected to change from time to time as we deem fit.
+
+LICENSE for all the Graphics from Freebies Section Only:
+
+   - You’re free to download, modify and use our graphics for your personal and commercial projects BUT you can’t Sell or Redistribute them as standalone files.
+   
+   - You can use these graphics on websites, blogs, printed on business cards, mugs or t-shirts, for facebook pages or any other custom work!
+ 
+   - No any attribution or link back to Pixaroma is need.
+
+You’re not allowed to:
+
+Resell, sub-license, license or redistribute these graphics to third parties. 
+You can’t sell these at sites like graphicriver/creativemarket/shutterstock/society6 or use these graphics for unlawful purposes, to infringe upon any copyright, to violate any person/identity or any other purpose that is not legal.
+You can’t use our graphics for company logo design (Because every logo should be unique).
+
+SHARING!
+
+You are free to share our resources by making a link to the particular page at our website. You are not allowed to make a direct link for download!
+
+SUPPORT:
+No support is offered by pixaroma.com for Freebies - but you can leave a message if you have any questions. We offer these graphics “as is”.
+
+Where you can find us:
+
+http://pixaroma.com/
+
+https://www.facebook.com/pixaroma
+https://www.youtube.com/c/PixaromaDesign
+https://twitter.com/pixaroma
+https://www.instagram.com/pixaroma/
+https://plus.google.com/+PixaromaDesign
+
+https://dribbble.com/pixaroma
+https://www.behance.net/pixaroma
+
+https://creativemarket.com/pixaroma
+https://designbundles.net/pixaroma
+https://thehungryjpeg.com/pixaroma/
+https://www.shutterstock.com/g/pixaroma
+https://www.fotolia.com/p/205455232
